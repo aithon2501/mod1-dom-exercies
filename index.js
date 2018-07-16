41550da25f4a1c21a3d435911644d78c3e39fa91
joshuajhun <joshuajhun@gmail.com>
2018-07-16 13:50:42 -0600

    inital commit with sudo code added to first challenge

---
+++ index.js
@@ -0,0 +1,7 @@
+// Use javascript to target the input field on the page 
+// Once you've targeted the input field add an event listner for a keyup event
+// once you've done console.log the event. 
+// once you've dont that console.log the event.target
+// once you've dont that console.log the event.target.value
+//
+// What is the difference between
