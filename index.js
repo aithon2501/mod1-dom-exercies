1d9111f0bf3c1d18cdc463e7a2d9de843ea9217b
joshuajhun <joshuajhun@gmail.com>
2018-07-18 09:18:47 -0600

    exercise four psudeo code added and previous solution

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-// Alright it's time for our next exercise
-  // this exercise will be like the previous one but the only difference 
-  // is that instead of logging the input's value on a click
-  // you're going to append the value of the input field onto a <ul> on the DOM
-  // and clear whatever text is inside of the input field.
-  
-// potential solution (previous exercise): --------------------
-//var submitButton = document.querySelector('.submit-button'); |
-//                                                             |
-//submitButton.addEventListener('click', function(event) {     |
-//  var inputField = document.querySelector('.text-input');    |
-//  console.log(inputField.value)                              |
-//});                                                          |
-//                                                             |
+// Now that you are able to a single listItem to the dom lets step it up.
+  // What I'd like you to do now is add another input field to the dom
+  // once the user clicks on the submit button add both of the inputs together 
+  // and append that value onto the dom.
+
+// potential solution (previous exercise): -------------------
+//                                                            |
+//var submitButton = document.querySelector('.submit-button');|
+//                                                            |
+//submitButton.addEventListener('click', function(event) {    |
+// var inputField = document.querySelector('.text-input');    |
+//  var listItems = document.querySelector('.list-items');    |
+//  var listItem = document.createElement('li');              |
+//  listItem.innerHTML = inputField.value;                    |
+//  listItems.append(listItem);                               |
+//  inputField.value = '';                                    |
+//});                                                         |
 // ------------------------------------------------------------
