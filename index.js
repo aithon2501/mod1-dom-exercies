71c88ba0dd168cdf59f1c717cfac7fe35079bef5
joshuajhun <joshuajhun@gmail.com>
2018-07-19 09:31:58 -0600

    add final exercies

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-// Now that we can append both values onto the dom lets look at 
-// Disabeling the submit button unless there is valid content in both input fields.
+// You made it to the last exercise! 
+// All you have to do now is make sure you can actually save ideas into localStorage. 
+// Even better if you can have it so that the ideas persist after a refresh. 
 
-/// ---- potential answer (previous exercise) ------------------
-//var submitButton = document.querySelector('.submit-button');|
-
+// ---- potential answer (previous exercise) ------------------
+//var submitButton = document.querySelector('.submit-button');
+//var titleInput = document.querySelector('.title');
+//var bodyInput = document.querySelector('.body');
+//
 //function formatIdea(title, body) {
 //  var listItem = document.createElement('li');
 //  var ideaText = title.value + '\n' + body.value
-//  return listItem.innerHTML = ideaText;
+//  listItem.innerHTML = ideaText;
+//
+//  return listItem;
 //};
 //
 //function addToDom(formatedIdea) {
-//  document.querySelector('.list-items').append(formatedIdea)
-//}
+//  document.querySelector('.list-items').append(formatedIdea);
+//};
 //
 //function resetInputs(title, body) {
 //  title.value = '';
 //  body.value = '';
 //}
+//
+//function toggleInputs() {
+//  bodyInput.disabled = !titleInput.value || false
+//  submitButton.disabled = !bodyInput.value || false
+//};
+//
+//
+//titleInput.addEventListener('keyup', toggleInputs);
+//bodyInput.addEventListener('keyup', toggleInputs);
 //
 //submitButton.addEventListener('click', function(event) {    
 //  var title = document.querySelector('.title');    
@@ -25,5 +39,5 @@
 //  var formatedIdea = formatIdea(title, body);
 //  addToDom(formatedIdea);
 //  resetInputs(title, body);
+//  toggleInputs();
 //});
-//--------------------------------------------------------------
